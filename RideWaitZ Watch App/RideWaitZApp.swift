29be32184d8d8ad4427f5d0bc31478745aede8c9
Marcus De La Garza <delag010@gmail.com>
2024-06-10 08:26:00 -0400

    Initial Commit

---
+++ RideWaitZ Watch App/RideWaitZApp.swift
@@ -0,0 +1,17 @@
+//
+//  RideWaitZApp.swift
+//  RideWaitZ Watch App
+//
+//  Created by Marcus De La Garza on 6/10/24.
+//
+
+import SwiftUI
+
+@main
+struct RideWaitZ_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
