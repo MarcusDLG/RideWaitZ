3e03fedd3ab30349220eda3084c4775fa72a9500
Marcus De La Garza <delag010@gmail.com>
2024-12-17 15:35:40 -0500

    Complication - Full Color, Tintable up next (#6)
    * Initial branch for complication work. Trying to be better about PRs!
    
    * Complication Rebuild
    
    * Full-color complicaiton complete. Errors corrected

---
+++ RideWaitzLauncher/AppIntent.swift
@@ -0,0 +1,18 @@
+//
+//  AppIntent.swift
+//  RideWaitzLauncher
+//
+//  Created by Marcus De La Garza on 12/17/24.
+//
+
+import WidgetKit
+import AppIntents
+
+struct ConfigurationAppIntent: WidgetConfigurationIntent {
+    static var title: LocalizedStringResource { "Configuration" }
+    static var description: IntentDescription { "This is an example widget." }
+
+    // An example configurable parameter.
+    @Parameter(title: "Favorite Emoji", default: "😃")
+    var favoriteEmoji: String
+}
---
+++ RideWaitzLauncher/RideWaitzLauncher.swift
@@ -0,0 +1,48 @@
+import WidgetKit
+import SwiftUI
+
+@main
+struct RideWaitzLauncher: Widget {
+    let kind: String = "RideWaitzLauncher"
+
+    var body: some WidgetConfiguration {
+        StaticConfiguration(kind: kind, provider: Provider()) { entry in
+            RideWaitzLauncherView(entry: entry)
+        }
+        .supportedFamilies([.accessoryCircular, .accessoryRectangular, .accessoryInline])
+        .configurationDisplayName("RideWaitZ Launcher")
+        .description("Launch RideWaitZ quickly.")
+    }
+}
+
+struct Provider: TimelineProvider {
+    func placeholder(in context: Context) -> SimpleEntry {
+        SimpleEntry(date: Date())
+    }
+
+    func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> Void) {
+        let entry = SimpleEntry(date: Date())
+        completion(entry)
+    }
+
+    func getTimeline(in context: Context, completion: @escaping (Timeline<SimpleEntry>) -> Void) {
+        let timeline = Timeline(entries: [SimpleEntry(date: Date())], policy: .never)
+        completion(timeline)
+    }
+}
+
+struct SimpleEntry: TimelineEntry {
+    let date: Date
+}
+
+struct RideWaitzLauncherView: View {
+    var entry: SimpleEntry
+
+    var body: some View {
+        ZStack {
+            Image("LauncherLogoColor")
+                .resizable()
+                .scaledToFit()
+        }
+    }
+}
